ff3a87dc43485adef502170e11bc61ca91a3c16c
Luis Solier Sajami <luis.sajami@gmail.com>
2019-11-02 12:15:47 -0500

    init commit

---
+++ src/main/java/com/lsolier/udacity/princingservice/PrincingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.lsolier.udacity.princingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrincingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrincingServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lsolier/udacity/princingservice/PrincingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lsolier.udacity.princingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrincingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
